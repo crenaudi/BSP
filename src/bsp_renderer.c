0407b985cd5f1237d20dd35a1eeeb1d8eb281bef
camille renaudin <camillerenaudin@mbpdecamille2.home>
2020-08-18 19:14:48 +0200

    clean

--- src/bsp_renderer.c
+++ src/bsp_renderer.c
@@ -32,14 +32,11 @@ void walk_tree(t_bspnode *node, t_cam2d c, t_lstl *p_lines, t_vecf2 depth)
     int side;
 
     side = pointonside(c.dvl_lr.p, &node->divline);
-	//printf("line %d side %d\n", node->line.linedef, side);
-
     if (p_lines->count < 255 && side != -1)
         if (node->side[side] != NULL)
             walk_tree(node->side[side], c, p_lines, depth);
     if (seg_onview(c, &node->line, depth) == SUCCESS)
     {
-		printf("line %d\n", node->line.linedef);
 		cpyl(&p_lines->lst[p_lines->count], &node->line);
         p_lines->count++;
     }
@@ -57,43 +54,5 @@ void    bsp_renderer(t_engine *e, t_player *pl, t_bspnode *node)
 	depth.x = pl->cam.dvl_lr.p.x + pl->cam.dvl_lr.dx * pl->cam.depth;
 	depth.y = pl->cam.dvl_lr.p.y + pl->cam.dvl_lr.dy * pl->cam.depth;
 	walk_tree(node, pl->cam, &p_lines, depth);
-	printf("\n");
 	raycast(e, &p_lines);
 }
-
-
-/*
-void WalkTree(t_bspnode *node)
-{
-   t_bspnode *nodestack[256];
-   t_bspnode **ptr_nodestack;
-   int side;
-
-   // Make sure the tree isn’t empty
-   if (node != NULL)
-   {
-      nodestack[0] = NULL;
-      ptr_nodeStack = nodestack + 1;
-      for (;;)
-      {
-         while (node[side] != NULL)
-         {
-            side = pointonside((t_vecf2){pl->coord_x, pl->coord_y}, &node->divline);
-            *ptr_nodeStack++ = node;
-            node = node[side];
-         }
-         //node est donc le point le plus proche devant
-         for (;;)
-         {
-            Visit(node);
-            if (node[side ^ 1] != NULL)
-            {
-               node = node[side ^ 1];
-               break;
-            }
-            if ((node = * —nodeStack) == NULL)
-               return;
-         }
-      }
-   }
-}*/
